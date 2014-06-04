0dd723a2b809c9eae33de0f463b0494d1c315b7b
Andy Song <keechi@qq.com>
2014-06-04 10:06:11 +0800

    Update on.js

--- on.js
+++ on.js
@@ -1,5 +1,6 @@
 Element.prototype.on = Element.prototype.addEventListener;
 NodeList.prototype.on = function (event, fn) {
+    // Array.prototype.forEach.call same
     []['forEach'].call(this, function (el) {
         el.on(event, fn);
     });
