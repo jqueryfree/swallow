8dc399323967f432984f591d42a0508fcffa34f9
Andy Song <keechi@qq.com>
2014-06-04 10:07:01 +0800

    Create query.js

---
+++ query.js
@@ -0,0 +1,5 @@
+var $ = document.querySelectorAll.bind(document);
+var $$ = document.getElementById.bind(document);
+Element.prototype.findone = function(str) {
+    return this.querySelector(str);
+};
