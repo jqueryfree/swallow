44b38f87b3f87d5a22a0412a7c81714e1ba9fb6b
Andy Song <keechi@qq.com>
2014-06-30 15:35:04 +0800

    Create trigger.js

---
+++ trigger.js
@@ -0,0 +1,16 @@
+Element.prototype.trigger = function (type, data) {
+    var event = document.createEvent('HTMLEvents');
+    event.initEvent(type, true, true);
+    event.data = data || {};
+    event.eventName = type;
+    event.target = this;
+    this.dispatchEvent(event);
+    return this;
+};
+
+NodeList.prototype.trigger = function (event) {
+    []['forEach'].call(this, function (el) {
+        el['trigger'](event);
+    });
+    return this;
+};
